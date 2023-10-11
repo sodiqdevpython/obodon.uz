39d4c9fee49f2481769bc3a016f233197c0e37ed
Sodiq <sodiq7898@gmail.com>
2023-10-11 08:20:59 +0500

    adding extra things

--- asosiy/models.py
+++ asosiy/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Valantyorlar(models.Model):
+    fio = models.CharField(max_length=250)
+    image = models.ImageField(upload_to='media/')
+    text = models.CharField(max_length=500)
